29d4a5305bdec397717733c15d6cdfdc162670a7
Jeswin Kumar <jeswinpk@agilehead.com>
2018-05-10 13:17:21 +0530

    WIP

--- src/topics/index.ts
+++ src/topics/index.ts
@@ -1,6 +1,4 @@
-import { IApplicationState, IRegexParseResult, regexParse } from "wild-yak";
-
-import * as publish from "./publish";
+import { IEvalState, IHandlerResult, ITopic, TopicBase } from "wild-yak";
 
 export interface IMessage {
   timestamp?: number;
@@ -8,12 +6,65 @@ export interface IMessage {
 }
 
 export interface IUserData {
-  x: number;
+  username: string;
+  session: string;
 }
 
-export function regex<TContextData, TUserData>(patterns: Array<RegExp>) {
-  return regexParse<TContextData, IMessage, TUserData>(patterns, x => x.text);
+export interface IHost {
+  getUserDirectory(username: string): string;
 }
 
-const topics: any = [];
-export default topics;
+export type ResultType = string | number | undefined;
+
+
+export class RootTopic extends TopicBase<IMessage, ResultType, IUserData, IHost>
+  implements ITopic<IMessage, ResultType, IUserData, IHost> {
+  async handle(
+    state: IEvalState<IMessage, ResultType, IUserData, IHost>,
+    message: IMessage,
+    userData: IUserData,
+    host: IHost
+  ): Promise<IHandlerResult<ResultType>> {
+    if (message.text === "do basic math") {
+      this.enterTopic(state, new MathTopic());
+      return { handled: true, result: "You can type a math expression" };
+    } else if (message.text === "help") {
+      this.enterTopic(state, new HelpTopic());
+      return {
+        handled: true,
+        result: "You're entering help mode. Type anything."
+      };
+    } else if (message.text === "reset password") {
+      this.enterTopic(state, new PasswordResetTopic());
+      return {
+        handled: true,
+        result: "Set your password."
+      };
+    } else {
+      return {
+        handled: true,
+        result:
+          "Life is like riding a bicycle. To keep your balance you must keep moving."
+      };
+    }
+  }
+}
+
+export class DefaultTopic
+  extends TopicBase<IMessage, ResultType, IUserData, IHost>
+  implements ITopic<IMessage, ResultType, IUserData, IHost> {
+  async handle(
+    state: IEvalState<IMessage, ResultType, IUserData, IHost>,
+    message: IMessage,
+    userData: IUserData,
+    host: IHost
+  ): Promise<IHandlerResult<ResultType>> {
+    return message.text === "hello world"
+      ? { handled: true, result: "greetings comrade!" }
+      : { handled: false };
+  }
+
+  isTopLevel() {
+    return true;
+  }
+}
--- src/topics/publish.ts
+++ src/topics/publish.ts
@@ -1,36 +1,31 @@
-import { createCondition, createTopic, ITopic } from "wild-yak";
 
-import { IMessage, IUserData, regex } from "./";
+export class MathTopic extends TopicBase<IMessage, ResultType, IUserData, IHost>
+  implements ITopic<IMessage, ResultType, IUserData, IHost> {
+  async handle(
+    state: IEvalState<IMessage, ResultType, IUserData, IHost>,
+    message: IMessage,
+    userData: IUserData,
+    host: IHost
+  ): Promise<IHandlerResult<ResultType>> {
+    if (message.text === "do advanced math") {
+      this.enterTopic(state, new AdvancedMathTopic());
+      return {
+        handled: true,
+        result: "You can do advanced math now."
+      };
+    } else {
+      const result = basicMathOperators(message.text);
+      return {
+        handled: true,
+        result:
+          typeof result !== "undefined"
+            ? result
+            : "I don't know how to handle this."
+      };
+    }
+  }
 
-export const topic = createTopic<IMessage, IUserData>()(
-  "publish",
-  async (args, userData) => {}
-)({
-  conditions: [
-    createCondition("publish", regex([]), async (state, { matches }) => {
-      return "hey, what's up!";
-    })
-  ]
-});
-
-/*
-  Get an id for yourself
-  @scuttlespace publish as jeswin
-
-  Publish as a post with url (with url scuttle.space/jeswin/about-scuttlekit)
-  @scuttlespace publish %/0AC79/Exv9w7CZBNhT1ikOIiTqvulaIRBXIedpwo/g=.sha256 at about-scuttlekit
-
-  Publish a multi-part post to scuttle.space/jeswin/about-scuttlekit
-  @scuttlespace publish %/0AC79/Exv9w7CZBNhT1ikOIiTqvulaIRBXIedpwo/g=.sha256 %/39AvPNoE3IqkDD6hhbwwUxCsSBWmAqaJU61HEAN+00=.sha256 at about-scuttlekit
-
-  Publish a post along with replies 
-  @scuttlespace publish thread %/0AC79/Exv9w7CZBNhT1ikOIiTqvulaIRBXIedpwo/g=.sha256 at about-scuttlekit
-
-  Unpublish
-  @scuttlespace unpublish %/0AC79/Exv9w7CZBNhT1ikOIiTqvulaIRBXIedpwo/g=.sha256
-
-  Unpublish with url
-  @scuttlespace unpublish about-scuttlekit
-*/
-
-export const conditions = [] as any;
+  isTopLevel() {
+    return true;
+  }
+}

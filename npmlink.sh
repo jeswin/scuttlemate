0ab4e74ab0ba5ae481ca1d6f7e0d3dbec625ba79
Jeswin Kumar <jeswinpk@agilehead.com>
2018-08-18 19:05:53 +0530

    Switch to wireman

--- npmlink.sh
+++
@@ -1,5 +0,0 @@
-rm -rf ../scuttlespace-commands-common/node_modules
-npm link ../scuttlespace-commands-common
-
-rm -rf ../scuttlespace-commands-user/node_modules
-npm link ../scuttlespace-commands-user
